b1fe481dfcd52ebdfcc3a656c8315cbee7a5e38e
FugaTakata <maple1213goo@gmail.com>
2020-04-20 10:21:24 +0900

    update template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define REP(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define REPD(i, n) for (ll i = (ll)(n)-1; i >= 0; i--)
+#define FOR(i, a, b) for (ll i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (ll i = (a); i >= (b); i--)
+#define ALL(x) (x).begin(), (x).end()
+#define MAX(x) *max_element(ALL(x))
+#define MIN(x) *min_element(ALL(x))
+#define INF 1000000000000
+#define MOD 1000000007
+
+void solve() {
+}
+
+signed main() {
+  solve();
+
+  return 0;
+}
